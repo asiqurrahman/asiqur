d0b69f4738e2857846caf3b2be4553e51a8c4219
asiqurrahman <asiqurrahman13@gmail.com>
2023-12-16 03:43:31 -0500

    remove boiler plates

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About page",
+  description: "Some playground",
+};
+
+
+const About = () => {
+  return <div>about</div>;
+};
+
+export default About;
